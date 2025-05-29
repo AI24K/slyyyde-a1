b2ef5a2f6358537686fc8658969b63a91ad1a8dd
AI24K <163212114+AI24K@users.noreply.github.com>
2025-05-29 23:35:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/(chat)/layout.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { SidebarProvider } from "ui/sidebar";
+import { AppSidebar } from "@/components/layouts/app-sidebar";
+import { AppHeader } from "@/components/layouts/app-header";
+import { KeyboardShortcutsPopup } from "@/components/keyboard-shortcuts-popup";
+import { ChatPreferencesPopup } from "@/components/chat-preferences-popup";
+import { appStore } from "../store";
+import { useShallow } from "zustand/shallow";
+import { useEffect } from "react";
+import { isShortcutEvent, Shortcuts } from "@/lib/keyboard-shortcuts";
+import { VoiceChatBot } from "@/components/voice-chat-bot";
+import { authClient } from "auth/client";
+import { useLatest } from "@/hooks/use-latest";
+
+export default function ChatLayout({
+  children,
+}: { children: React.ReactNode }) {
+  const { data, refetch } = authClient.useSession();
+  const latestSessionApi = useLatest({ data, refetch });
+  const [openChatPreferences, openShortcutsPopup, appStoreMutate] = appStore(
+    useShallow((state) => [
+      state.openChatPreferences,
+      state.openShortcutsPopup,
+      state.mutate,
+    ]),
+  );
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const isShortcutPopupEvent = isShortcutEvent(
+        e,
+        Shortcuts.openShortcutsPopup,
+      );
+      const isChatPreferencesEvent = isShortcutEvent(
+        e,
+        Shortcuts.openChatPreferences,
+      );
+      if (!isShortcutPopupEvent && !isChatPreferencesEvent) return;
+      e.preventDefault();
+      e.stopPropagation();
+      if (isShortcutPopupEvent) {
+        appStoreMutate({ openShortcutsPopup: true });
+      }
+      if (isChatPreferencesEvent) {
+        appStoreMutate({ openChatPreferences: true });
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!latestSessionApi.current.data?.session) {
+        latestSessionApi.current.refetch();
+      }
+    }, 5000);
+  }, []);
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="relative w-full flex flex-col h-screen">
+        <AppHeader />
+        <div className="flex-1 overflow-y-auto">{children}</div>
+      </main>
+      <KeyboardShortcutsPopup
+        open={openShortcutsPopup}
+        onOpenChange={(open) => appStoreMutate({ openShortcutsPopup: open })}
+      />
+      <ChatPreferencesPopup
+        open={openChatPreferences}
+        onOpenChange={(open) => appStoreMutate({ openChatPreferences: open })}
+      />
+      <VoiceChatBot />
+    </SidebarProvider>
+  );
+}
---
+++ src/components/layouts/app-sidebar.tsx
@@ -0,0 +1,84 @@
+"use client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "ui/sidebar";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { getStorageManager } from "lib/browser-stroage";
+import { AppSidebarMenus } from "./app-sidebar-menus";
+import { AppSidebarProjects } from "./app-sidebar-projects";
+import { AppSidebarThreads } from "./app-sidebar-threads";
+import { AppSidebarUser } from "./app-sidebar-user";
+import { MCPIcon } from "ui/mcp-icon";
+import { isShortcutEvent, Shortcuts } from "lib/keyboard-shortcuts";
+
+const browserSidebarStorage = getStorageManager<boolean>("sidebar_state");
+
+export function AppSidebar() {
+  const { open, toggleSidebar } = useSidebar();
+  const router = useRouter();
+
+  // persist sidebar state
+  useEffect(() => {
+    browserSidebarStorage.set(open);
+  }, [open]);
+
+  // global shortcuts
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (isShortcutEvent(e, Shortcuts.openNewChat)) {
+        e.preventDefault();
+        router.push("/");
+        router.refresh();
+      }
+      if (isShortcutEvent(e, Shortcuts.toggleSidebar)) {
+        e.preventDefault();
+        toggleSidebar();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [router, toggleSidebar]);
+
+  return (
+    <Sidebar collapsible="offcanvas">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center gap-0.5">
+            <SidebarMenuButton asChild>
+              <Link
+                href={`/`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  router.push("/");
+                  router.refresh();
+                }}
+              >
+                <MCPIcon className="size-4 fill-foreground" />
+                <h4 className="font-bold">mcp/chat-bot</h4>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent className="mt-6">
+        <AppSidebarMenus isOpen={open} />
+        <AppSidebarProjects />
+        <AppSidebarThreads />
+      </SidebarContent>
+
+      <SidebarFooter className="flex flex-col items-stretch space-y-2">
+        <AppSidebarUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
